da56ea1b6ccbde08b35f695af91b6820b1afbd4f
boiroibrahima46-sys <boiroibrahima46@gmail.com>
2026-02-09 01:08:43 +0100

    Add files via upload

---
+++ MonCV.py
@@ -0,0 +1,63 @@
+import streamlit as st
+st.markdown(""" # Climatologue et Géomaticien""")
+st.markdown("""
+
+Je suis étudiant, maître en climatologie au département de Géographie(Science de l'homme et de la Socièté) et capable d'analyser des données climatiques de toute nature. Titulaire d'un Brevet de Technicien Supérieur(BTS) en Géomatique, j'ai la capacité de réaliser des produits géomatiques de toute sorte. 
+""")
+
+
+with st.sidebar:
+    st.markdown(""" # Nom et Prénom: BOIRO Ibrahima
+    """)
+    st.markdown(""" # Téléphone: 783954234
+    """)
+    st.markdown(""" # Email: boiroibrahima46@gmail.com
+    """)
+    st.markdown("""# Nationalité: Sénégalaise
+    """)
+    st.markdown(""" # Adresse: UCAD
+    """)
+    st.markdown(""" # Formations:
+
+    # Brevet de Technicien Supérieur en Géomatique (CEDT/LE G15)
+    # Maitre en Climatologie(UCAD)
+    # Licence en Géographie(UCAD)
+    # Baccalauréat(Lycée de Dabo)
+    """)    
+st.title("Compétences")
+st.markdown("""  ARCGIS : 
+cartes de localisation, 
+Produire une carte thématique des types de sol, 
+Généner un Modèle Numérique de Terrain(MTN), 
+Créer une Base de Données, 
+générer des lignes d'eau, 
+calculs de la géométrie,
+Importer des données de type texte
+Corriger des erreurs de topologie """) 
+st.markdown("""  QANTUMGIS : 
+digitalisation(Arbre,route,bati), calculs de la géométrie, création de base de données, manipulation de données vecteur et raster""")
+st.markdown(""" SUITE BUREAUTIQUE :
+Word,
+Excel,
+Power_Point""")
+st.markdown(""" PYTHON :
+(IDLE, STREAMLIT, Notebook)
+""")
+st.markdown("""  DESSIN DE PLAN :
+Réaliser des projets 2D sur AUTOCAD et 3D sur SketchUp """)
+st.markdown("""TOPOGRAPHIE :
+Effectuer des levés topographiques corrects des informations
+Calculer des angles intérieurs et extérieurs sur AUTOCAD
+""")
+st.title("Expériences professionnelles")
+st.markdown(""" Mener des opérations de terrain pour collecter des données géographiques(informations géographiques) avec "MOBILE TOPOGRAPHER" """)
+
+
+
+
+
+  
+    
+    
+    
+    
